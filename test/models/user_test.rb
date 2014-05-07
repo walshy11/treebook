70a909992fd86c2e85a0b3aee493965936048c28
Ted Smith <ted.smith11@gmail.com>
2014-05-07 09:20:37 -0700

    Added test for user first name, last name, and profile correctness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	class UserTest < ActiveSupport::TestCase
+	test "A user should enter a first name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end
+
+	test "A user should enter a last name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+	test "A user should enter a profile name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a unique profile name" do
+		user = User.new
+		user.profile_name = users(:ted).profile_name
+		users(:ted)
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a profile name without spaces" do
+		user = User.new
+		user.profile_name ="My Profile Name with Spaces"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+
 end
